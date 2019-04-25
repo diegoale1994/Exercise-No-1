3e07edf26c4171d26bc24c87b19435de7e175193
Diego Alejandro <diego.fc.1@hotmail.com>
2019-04-25 18:35:14 -0500

    first commit

---
+++ src/main/java/com/rexsoft/clinic/app/EjercicioNo1Application.java
@@ -0,0 +1,13 @@
+package com.rexsoft.clinic.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioNo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioNo1Application.class, args);
+	}
+
+}
